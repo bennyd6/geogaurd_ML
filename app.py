37fa22a655e3387b8cab6521a0afcbb8e437a9d9
bennyd6 <228r1a66d6@cmrec.ac.in>
2024-11-10 12:15:16 +0530

    added os

--- app.py
+++ app.py
@@ -82,4 +82,6 @@ def predict():
 
 # Run the app
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))  # Default to 5000 if no PORT env var is set
+    app.run(host='0.0.0.0', port=port, debug=True)
